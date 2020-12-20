95e821e4aa94090f500b4b41019c182cd93a27c3
luolianfeng <luolianfeng@qq.com>
2020-12-20 10:58:53 +0800

    移除删选按钮

--- app/src/main/java/com/example/newbiechen/ireader/ui/activity/BookListActivity.java
+++ app/src/main/java/com/example/newbiechen/ireader/ui/activity/BookListActivity.java
@@ -4,10 +4,6 @@ import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.CheckBox;
 
 import com.example.newbiechen.ireader.R;
 import com.example.newbiechen.ireader.RxBus;
@@ -16,13 +12,11 @@ import com.example.newbiechen.ireader.model.bean.BookTagBean;
 import com.example.newbiechen.ireader.model.flag.BookListType;
 import com.example.newbiechen.ireader.model.remote.RemoteRepository;
 import com.example.newbiechen.ireader.ui.adapter.HorizonTagAdapter;
-import com.example.newbiechen.ireader.ui.adapter.TagGroupAdapter;
 import com.example.newbiechen.ireader.ui.base.BaseTabActivity;
 import com.example.newbiechen.ireader.ui.fragment.BookListFragment;
 import com.example.newbiechen.ireader.utils.LogUtils;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import butterknife.BindView;
@@ -39,15 +33,8 @@ public class BookListActivity extends BaseTabActivity {
     private static final int RANDOM_COUNT = 5;
     @BindView(R.id.book_list_rv_tag_horizon)
     RecyclerView mRvTag;
-    @BindView(R.id.book_list_cb_filter)
-    CheckBox mCbFilter;
-    @BindView(R.id.book_list_rv_tag_filter)
-    RecyclerView mRvFilter;
     /*************************************/
     private HorizonTagAdapter mHorizonTagAdapter;
-    private TagGroupAdapter mTagGroupAdapter;
-    private Animation mTopInAnim;
-    private Animation mTopOutAnim;
     /************Params*******************/
     private final CompositeDisposable mDisposable = new CompositeDisposable();
     @Override
@@ -91,10 +78,6 @@ public class BookListActivity extends BaseTabActivity {
         LinearLayoutManager tagManager = new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false);
         mRvTag.setLayoutManager(tagManager);
         mRvTag.setAdapter(mHorizonTagAdapter);
-
-        //筛选框
-        mTagGroupAdapter = new TagGroupAdapter(mRvFilter,4);
-        mRvFilter.setAdapter(mTagGroupAdapter);
     }
 
     @Override
@@ -108,48 +91,6 @@ public class BookListActivity extends BaseTabActivity {
                 }
         );
 
-        //筛选
-        mCbFilter.setOnCheckedChangeListener(
-                (btn,checked)->{
-                    if (mTopInAnim == null || mTopOutAnim == null){
-                        mTopInAnim = AnimationUtils.loadAnimation(this,R.anim.slide_top_in);
-                        mTopOutAnim = AnimationUtils.loadAnimation(this,R.anim.slide_top_out);
-                    }
-
-                    if (checked){
-                        mRvFilter.setVisibility(View.VISIBLE);
-                        mRvFilter.startAnimation(mTopInAnim);
-                    }
-                    else {
-                        mRvFilter.startAnimation(mTopOutAnim);
-                        mRvFilter.setVisibility(View.GONE);
-                    }
-                }
-        );
-
-        //筛选列表
-        mTagGroupAdapter.setOnChildItemListener(
-                (view, groupPos, childPos) -> {
-                    String bean = mTagGroupAdapter.getChildItem(groupPos, childPos);
-                    //是否已存在
-                    List<String> tags =  mHorizonTagAdapter.getItems();
-                    boolean isExist = false;
-                    for (int i=0; i<tags.size(); ++i){
-                        if (bean.equals(tags.get(i))){
-                            mHorizonTagAdapter.setCurrentSelected(i);
-                            mRvTag.getLayoutManager().scrollToPosition(i);
-                            isExist = true;
-                        }
-                    }
-                    if (!isExist){
-                        //添加到1的位置,保证全本的位置
-                        mHorizonTagAdapter.addItem(1,bean);
-                        mHorizonTagAdapter.setCurrentSelected(1);
-                        mRvTag.getLayoutManager().scrollToPosition(1);
-                    }
-                    mCbFilter.setChecked(false);
-                }
-        );
     }
 
 
@@ -167,7 +108,6 @@ public class BookListActivity extends BaseTabActivity {
                 .subscribe(
                         (tagBeans)-> {
                             refreshHorizonTag(tagBeans);
-                            refreshGroupTag(tagBeans);
                         },
                         (e) ->{
                             LogUtils.e(e);
@@ -199,15 +139,5 @@ public class BookListActivity extends BaseTabActivity {
             }
         }
         mHorizonTagAdapter.addItems(randomTag);
-    }
-
-    private void refreshGroupTag(List<BookTagBean> tagBeans){
-        //由于数据还有根据性别分配，所以需要加上去
-        BookTagBean bean = new BookTagBean();
-        bean.setName(getResources().getString(R.string.nb_tag_sex));
-        bean.setTags(Arrays.asList(getResources().getString(R.string.nb_tag_boy),getResources().getString(R.string.nb_tag_girl)));
-        tagBeans.add(0,bean);
-
-        mTagGroupAdapter.refreshItems(tagBeans);
     }
 }
