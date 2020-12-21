93375f05c69eff50adcedbf7a6878c9c7caead03
luolianfeng <luolianfeng@upchina.com>
2020-12-21 11:31:34 +0800

    代码调整

--- app/src/main/java/com/example/newbiechen/ireader/activity/MainActivity.java
+++ app/src/main/java/com/example/newbiechen/ireader/activity/MainActivity.java
@@ -1,13 +1,9 @@
 package com.example.newbiechen.ireader.activity;
 
-import android.Manifest;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.os.Build;
 import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.view.menu.MenuBuilder;
 import android.support.v7.widget.LinearLayoutManager;
@@ -30,9 +26,8 @@ import com.example.newbiechen.ireader.presenter.BookShelfPresenter;
 import com.example.newbiechen.ireader.presenter.contract.BookShelfContract;
 import com.example.newbiechen.ireader.ui.adapter.CollBookAdapter;
 import com.example.newbiechen.ireader.ui.base.BaseMVPActivity;
-import com.example.newbiechen.ireader.utils.PermissionsChecker;
 import com.example.newbiechen.ireader.utils.RxUtils;
-import com.example.newbiechen.ireader.utils.ToastUtils;
+import com.example.newbiechen.ireader.utils.UPPermissionUtil;
 import com.example.newbiechen.ireader.widget.itemdecoration.DividerItemDecoration;
 import com.example.newbiechen.ireader.widget.refresh.ScrollRefreshRecyclerView;
 
@@ -48,14 +43,7 @@ public class MainActivity extends BaseMVPActivity<BookShelfContract.Presenter>
         implements BookShelfContract.View {
     /*************Constant**********/
     private static final int WAIT_INTERVAL = 2000;
-    private static final int PERMISSIONS_REQUEST_STORAGE = 1;
 
-    static final String[] PERMISSIONS = {
-            Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE
-    };
-    /***************Object*********************/
-    private PermissionsChecker mPermissionsChecker;
     /*****************Params*********************/
     private boolean isPrepareFinish = false;
 
@@ -86,7 +74,22 @@ public class MainActivity extends BaseMVPActivity<BookShelfContract.Presenter>
     protected void initWidget() {
         super.initWidget();
 
-        setUpAdapter();
+        if (UPPermissionUtil.checkPermissions(this)) {
+            setUpAdapter();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == UPPermissionUtil.CODE) {
+            if (UPPermissionUtil.checkResult(grantResults)) {
+                setUpAdapter();
+            } else {
+                Toast.makeText(this, "没有权限无法提供服务，拜拜了您", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        }
     }
 
     private void setUpAdapter() {
@@ -184,7 +187,6 @@ public class MainActivity extends BaseMVPActivity<BookShelfContract.Presenter>
     @Override
     protected void processLogic() {
         super.processLogic();
-        mRvContent.startRefresh();
     }
 
     private void openItemDialog(CollBookBean collBook) {
@@ -279,34 +281,8 @@ public class MainActivity extends BaseMVPActivity<BookShelfContract.Presenter>
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        Class<?> activityCls = null;
-        switch (id) {
-            case R.id.action_scan_local_book:
-
-                if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M) {
-
-                    if (mPermissionsChecker == null) {
-                        mPermissionsChecker = new PermissionsChecker(this);
-                    }
-
-                    //获取读取和写入SD卡的权限
-                    if (mPermissionsChecker.lacksPermissions(PERMISSIONS)) {
-                        //请求权限
-                        ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSIONS_REQUEST_STORAGE);
-                        return super.onOptionsItemSelected(item);
-                    }
-                }
-
-                activityCls = FileSystemActivity.class;
-                break;
-            default:
-                break;
-        }
-        if (activityCls != null) {
-            Intent intent = new Intent(this, activityCls);
-            startActivity(intent);
-        }
+        Intent intent = new Intent(this, FileSystemActivity.class);
+        startActivity(intent);
         return super.onOptionsItemSelected(item);
     }
 
@@ -323,25 +299,6 @@ public class MainActivity extends BaseMVPActivity<BookShelfContract.Presenter>
             }
         }
         return super.onPreparePanel(featureId, view, menu);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode) {
-            case PERMISSIONS_REQUEST_STORAGE: {
-                // 如果取消权限，则返回的值为0
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    //跳转到 FileSystemActivity
-                    Intent intent = new Intent(this, FileSystemActivity.class);
-                    startActivity(intent);
-
-                } else {
-                    ToastUtils.show("用户拒绝开启读写权限");
-                }
-            }
-        }
     }
 
     @Override
