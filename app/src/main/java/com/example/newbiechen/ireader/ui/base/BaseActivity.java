5a466b81b92b864a12aa14926abded8afc38fde4
luolianfeng <luolianfeng@qq.com>
2021-01-04 23:28:40 +0800

    依赖关系整理

--- app/src/main/java/com/example/newbiechen/ireader/ui/base/BaseActivity.java
+++ app/src/main/java/com/example/newbiechen/ireader/ui/base/BaseActivity.java
@@ -2,12 +2,12 @@ package com.example.newbiechen.ireader.ui.base;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.LayoutRes;
-import android.support.annotation.Nullable;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import androidx.annotation.LayoutRes;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.example.newbiechen.ireader.R;
 import com.example.newbiechen.ireader.utils.StatusBarCompat;
